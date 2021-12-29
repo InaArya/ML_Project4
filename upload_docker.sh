c40fe96e0e9145c084ef8ca6f473e6d56e1ef6fc
InaArya <95537907+InaArya@users.noreply.github.com>
2021-12-29 21:18:22 +0530

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=inaarya/path
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag api:latest $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
